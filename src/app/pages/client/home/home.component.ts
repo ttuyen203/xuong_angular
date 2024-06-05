0a162873e6334b3cc9438209ee651a6d089e5a59
ttuyen <totuyencv@gmail.com>
2024-06-05 23:31:00 +0700

    update

--- src/app/pages/client/home/home.component.ts
+++ src/app/pages/client/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Product } from '../../../types/product';
+import { ProductService } from '../../../services/product.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  products: Product[] = [];
+
+  productService = inject(ProductService);
+  router = inject(Router);
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getAll().subscribe(
+      (data: Product[]) => {
+        this.products = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
